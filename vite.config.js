74f65575f83f5ca6a3cc2afe69ede3d986f1ec52
unknown <maciek.smialowski@tlen.pl>
2025-02-03 16:57:32 +0100

    eleganckie popraweczki

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+  build: {
+    outDir: 'dist' // Upewnij się, że folder outputu to `dist`
+  },
+  server: {
+    port: 3000, // Możesz zmienić, jeśli port jest zajęty
+    host: true
   }
-});
+})
