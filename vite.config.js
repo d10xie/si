5e465f31c94c10d9ff5a475d2d77eed8fe87d044
unknown <maciek.smialowski@tlen.pl>
2025-02-03 16:59:41 +0100

    eleganckie popraweczki

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Upewnij się, że folder outputu to `dist`
+    outDir: 'dist'  // Vercel wymaga katalogu `dist`
   },
   server: {
-    port: 3000, // Możesz zmienić, jeśli port jest zajęty
     host: true
   }
 })
